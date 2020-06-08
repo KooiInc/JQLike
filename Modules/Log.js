9dfedcca84ad5d25d1b35c3f5cc557b295fee6fd
Renzo Kooi <renzo.kooi@gmail.com>
2020-06-08 12:22:30 +0200

    Use getters

--- Modules/Log.js
+++ Modules/Log.js
@@ -1,7 +1,7 @@
 import { fromHtml } from "./DOM.js";
 
 let useLogging = false;
-const debugLog = { on: () => useLogging = true, off: useLogging = false };
+const debugLog = { get on() { useLogging = true; }, get off() { useLogging = false; } };
 const log = txt => {
   if (!document.querySelector("#jql_logger")) {
     const logBlock = fromHtml(`<pre id="jql_logger"></pre>`, document.body);
